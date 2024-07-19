c60fe7042b09227ba7d9f2a20bea4471ec64bc5b
Fabien <fabien@feub.net>
2024-07-19 14:48:07 +0200

    Refactor: buttons colors

--- src/components/ButtonWithIcon.vue
+++ src/components/ButtonWithIcon.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 <template>
     <div>
         <button @click="$emit('showPart')"
-            :class="props.isShowing ? 'ring-feub-secondary ring-2 ring-offset-2 ring-offset-feub-secondary bg-feub-primary text-white dark:bg-feub-primary-dark hover:bg-feub-primary-dark/50' : 'bg-feub-primary text-feub-toned-down hover:bg-feub-secondary'"
+            :class="props.isShowing ? 'ring-2 ring-offset-2 ring-offset-slate-50 ring-feub-toned-down  bg-feub-primary hover:bg-feub-primary/90 text-white dark:ring-offset-feub-secondary-dark' : 'bg-feub-toned-down text-white hover:bg-feub-secondary/50 dark:bg-feub-primary-dark'"
             class="flex items-center gap-2 font-medium rounded-md p-2">
             <span>
                 <slot name="icon"></slot>
