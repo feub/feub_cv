89fda336bd065b272c606cfae19434acf3478e19
Fabien Amann <fabien@feub.net>
2025-03-28 17:46:14 +0100

    refactor: 2025 in the footer :)

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -5,7 +5,7 @@
         class="static bottom-0 m-5 p-3 bg-white dark:bg-feub-secondary-dark text-feub-toned-down dark:text-gray-400 rounded-xl text-sm font-medium">
         <div class="flex justify-between items-center">
             <div>Ce site est développé en Vue 3 + Tailwind CSS</div>
-            <div>2024</div>
+            <div>2025</div>
         </div>
     </footer>
 </template>
