8450dab7c98f87d3f508f1c52832cf069b554441
Fabien Amann <fabien@feub.net>
2025-03-28 17:46:43 +0100

    new CV

--- src/components/Sidebar/DownloadCvCard.vue
+++ src/components/Sidebar/DownloadCvCard.vue
@@ -1,25 +1,25 @@
 <script setup>
-import ContactCardItem from '@/components/Sidebar/ContactCardItem.vue';
-import { ArrowDownOnSquareIcon } from '@heroicons/vue/24/outline'
+import ContactCardItem from "@/components/Sidebar/ContactCardItem.vue";
+import { ArrowDownOnSquareIcon } from "@heroicons/vue/24/outline";
 </script>
 
 <template>
-    <div class="bg-white dark:bg-feub-secondary-dark rounded-xl p-3">
-        <h2 class="font-bold text-xl mb-4 dark:text-white">Télécharger CV</h2>
-        <div class="space-y-4">
-            <div class="flex items-center justify-between">
-                <div class="flex gap-3">
-                    <ArrowDownOnSquareIcon class="size-6 text-feub-primary" />
-                    <div class="text-sm">
-                        <div class="text-feub-primary dark:text-white font-medium">
-                            <a href="/docs/CV_2025-02-10_Fabien_AMANN.pdf" target="_blank">Cliquer ici</a>
-                        </div>
-                        <div class="text-gray-400">
-                            PDF
-                        </div>
-                    </div>
-                </div>
+  <div class="bg-white dark:bg-feub-secondary-dark rounded-xl p-3">
+    <h2 class="font-bold text-xl mb-4 dark:text-white">Télécharger CV</h2>
+    <div class="space-y-4">
+      <div class="flex items-center justify-between">
+        <div class="flex gap-3">
+          <ArrowDownOnSquareIcon class="size-6 text-feub-primary" />
+          <div class="text-sm">
+            <div class="text-feub-primary dark:text-white font-medium">
+              <a href="/docs/CV_2025-03-13_FABIEN_AMANN.pdf" target="_blank"
+                >Cliquer ici</a
+              >
             </div>
+            <div class="text-gray-400">PDF</div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
